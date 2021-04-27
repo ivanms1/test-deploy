09ec7dac420f163ba78a0d82c888d1172803bb8e
Ivan Saldano <ivanms1@gmail.com>
2021-04-27 16:38:17 +0900

    second commit

---
+++ electron/const/index.ts
@@ -0,0 +1,9 @@
+export const MANAGER_SERVER = "http://192.168.100.54:4000/api/v1";
+export const DRIVE_SERVER = "http://192.168.100.54:8000/api";
+
+export const ORG_NAME = "Org1";
+
+export const FcnTypes = {
+  CreateFile: "CreateFile",
+  LikeContent: "LikeContent",
+};
---
+++ src/const/index.ts
@@ -0,0 +1,14 @@
+export const serverUrl = "http://192.168.100.54:8000/api";
+
+export const AUTH_TOKEN = "conun-drive-auth-token";
+
+export const SAVED_SEARCHES = "conun-saved-searches";
+
+export const FcnTypes = {
+  Transfer: "Transfer",
+  Init: "Init",
+  Mint: "Mint",
+  Burn: "Burn",
+  GetDetails: "GetDetails",
+  BalanceOf: "BalanceOf",
+};
